c9260dd54dc0d401ce3e86e220362d32dacfbcf1
1 <semperidem1989@gmail.com>
2023-03-01 12:47:52 +0500

    bugs

---
+++ scripts/index.js
@@ -0,0 +1,51 @@
+const closeButtonPopup = document.querySelector('.popup__close')
+const editButton = document.querySelector('.profile__info-button-edit')
+const infoName = document.querySelector('.profile__info-name')
+const description = document.querySelector('.profile__info-description')
+    const popup = document.querySelector('.popup')
+    const form = document.querySelector('.popup')
+    const formName = document.querySelector('.popup__input_form_name')
+    const formDescription = document.querySelector('.popup__input_form_description')
+  
+
+  function closePopup(popup) {
+    popup.classList.remove('popup_opened');
+    popup.classList.add('popup_hide');
+  }
+  
+  function showPopup(popup) {
+    popup.classList.remove('popup_hide');
+    popup.classList.add('popup_opened');
+  }
+
+  
+  function openProfileInfoPopup() {
+    formName.value = infoName.textContent;
+    formDescription.value = description.textContent;
+    showPopup(popup);
+  }
+  
+  function saveInfo(e) {
+    e.preventDefault();
+    infoName.textContent = formName.value;
+    description.textContent = formDescription.value;
+    closePopup(popup);
+  }
+  
+  
+  
+  
+  function addListeners() {
+      
+    closeButtonPopup.addEventListener('click', () => {
+      closePopup(popup);
+    });
+  
+  
+    editButton.addEventListener('click', openProfileInfoPopup);  
+
+    form.addEventListener('submit', saveInfo);
+  
+  }
+  
+  addListeners();
